08a6430c50a8b04bb473ab75484f7be4d8ee8991
Hubert Popiołkiewicz <hubert.popiolkiewicz@gmail.com>
2015-09-11 17:59:00 +0200

    Poprawki 4fun

--- src/RenderPanel.java
+++ src/RenderPanel.java
@@ -4,43 +4,38 @@ import java.awt.Point;
 
 import javax.swing.JPanel;
 
+public class RenderPanel extends JPanel {
 
-public class RenderPanel extends JPanel
-{
-	
-	private static final long serialVersionUID = -1015558200090253783L;
-	
-	/**
-	 * Szerokoœæ pojedynczego pola.
-	 */
-	private static final int sP = 20;
-	/**
-	 * Wysokoœæ pojedynczego pola.
-	 */
-	private static final int wP = 20;
-	
-	protected void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		g.setColor(Color.LIGHT_GRAY);
-		
-		for(int i = 0; i<=600; i+=sP)
-			g.drawLine(i, 0, i, 600);
-		
-		for(int j = 0; j<=600; j+=wP)
-			g.drawLine(0, j , 600, j);
-		
-		Snake snake = Snake.snake;
-		
-		for(Point x: snake.segmentySnake)
-		{
-			g.setColor(Color.GREEN);
-			g.fillRect((int)x.getX() * sP, (int)x.getY() * wP, sP, wP);
-		}
-		
-		Point cherry = Snake.cherry;
-		
-		g.setColor(Color.RED);
-		g.fillRect((int)cherry.getX() * sP, (int)cherry.getY() * wP, sP, wP);
-	}
+    private static final long serialVersionUID = -1015558200090253783L;
+    private static final int sP = 20;
+    private static final int wP = 20;
+
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        redrawPanel(graphics);
+        redrawSnake(graphics);
+        redrawCherry(graphics);
+    }
+
+    private void redrawPanel(Graphics graphics) {
+        graphics.setColor(Color.LIGHT_GRAY);
+        for (int i = 0 ; i <= 600 ; i += sP)
+            graphics.drawLine(i, 0, i, 600);
+        for (int j = 0 ; j <= 600 ; j += wP)
+            graphics.drawLine(0, j, 600, j);
+    }
+
+    private void redrawSnake(Graphics graphics) {
+        Snake snake = Snake.getInstance();
+        for (Point x : snake.getSections()) {
+            graphics.setColor(Color.GREEN);
+            graphics.fillRect((int) x.getX() * sP, (int) x.getY() * wP, sP, wP);
+        }
+    }
+
+    private void redrawCherry(Graphics graphics) {
+        Point cherry = Snake.getCherry();
+        graphics.setColor(Color.RED);
+        graphics.fillRect((int) cherry.getX() * sP, (int) cherry.getY() * wP, sP, wP);
+    }
 }
--- src/Snake.java
+++ src/Snake.java
@@ -12,138 +12,135 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
-public class Snake implements ActionListener, KeyListener
-{
-	ArrayList<Point> segmentySnake = new ArrayList<Point>();
-	
-	public JFrame jFrame;
-	
-	public RenderPanel renderPanel;
-	
-	public static Snake snake;
-	
-	public Timer timer = new Timer(10, this);
+public class Snake implements ActionListener, KeyListener {
 
-	static char LEFT = 'a', RIGHT = 'd', DOWN = 's', UP = 'w', direction = DOWN;
-	
-	int ruch = 0;
-	
-	Point head, beforeHead;
-	
-	static Point cherry;
-	
-	Random random = new Random();
-	
-	public Snake()
-	{
-		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		jFrame = new JFrame("Snake");
-		jFrame.setSize(605,625);
-		jFrame.setLocation(dimension.width/2 - 300, dimension.height/2 - 300);
-		jFrame.setResizable(false);
-		jFrame.setVisible(true);
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.add(renderPanel = new RenderPanel());
-		jFrame.addKeyListener(this);
-		
-		segmentySnake.add(new Point(3, 2));
-		head = new Point(3, 3);
-		segmentySnake.add(head);
-		throwCherry(random, segmentySnake);
+    private static char LEFT = 'a', RIGHT = 'd', DOWN = 's', UP = 'w', direction = DOWN;
+    private static Point cherry;
+    private static Snake snake;
+    private static Random random = new Random();
+    private ArrayList<Point> sections = new ArrayList<Point>();
+    private JFrame jFrame;
+    private RenderPanel renderPanel;
+    private Timer timer = new Timer(10, this);
+    private Point head, beforeHead;
+    private int speedInterval = 0;
 
-		timer.start();
-	}
+    public Snake() {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        jFrame = new JFrame("Snake");
+        jFrame.setSize(605, 625);
+        jFrame.setLocation(dimension.width / 2 - 300, dimension.height / 2 - 300);
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(renderPanel = new RenderPanel());
+        jFrame.addKeyListener(this);
 
-	public static void restartGry(ArrayList<Point> vSegmenty)
-	{
-		vSegmenty.removeAll(vSegmenty);
-		vSegmenty.add(new Point(3, 2));
-		snake.head = new Point(3, 3);
-		vSegmenty.add(snake.head);
-		
-		snake.timer.start();
-		direction = DOWN;
-	}
-	
-	public static void throwCherry(Random vRandom, ArrayList<Point> vSegmenty)
-	{
-		cherry = new Point(vRandom.nextInt(30), vRandom.nextInt(30));
-		for(Point x : vSegmenty)	
-			if(cherry.getX() == x.getX() && cherry.getY() == x.getY())
-				throwCherry(vRandom, vSegmenty);
-	}
-	
-	public static void main(String[] args)
-	{
-		snake = new Snake();
-		
-		snake.timer.stop();
-		JOptionPane.showMessageDialog(null, "\t\tWitaj w grze SNAKE!"
-				+ "\nU¿ywaj nastêpuj¹cych klawiszy aby zmieniæ kierunek ruchu wê¿a:"
-				+ "\n W - do góry"
-				+ "\n S - w dó³ "
-				+ "\n A - w lewo"
-				+ "\n D - w prawo."
-				+ "\n POWODZENIA!");
-		snake.timer.start();
-	}
-	
-	public void actionPerformed(ActionEvent arg0)
-	{
-		renderPanel.repaint();
-		ruch++;
-		
-		if(ruch % 4 == 0)
-		{
-			if(direction == DOWN)
-				beforeHead = new Point((int)head.getX(), (int)head.getY() + 1);
-			if(direction == UP)
-				beforeHead = new Point((int)head.getX(), (int)head.getY() - 1);
-			if(direction == LEFT)
-				beforeHead = new Point((int)head.getX() - 1, (int)head.getY());
-			if(direction == RIGHT)
-				beforeHead = new Point((int)head.getX() + 1, (int)head.getY());
-			segmentySnake.add(beforeHead);
-			Point zastepczy = head;
-			head = beforeHead;
-			beforeHead = zastepczy;
-			
-			if(head.getX() == cherry.getX() && head.getY() == cherry.getY())
-			{
-				throwCherry(random, segmentySnake);
-			}
-			else
-			{
-			segmentySnake.remove(0);
-			}
+        sections.add(new Point(3, 2));
+        head = new Point(3, 3);
+        sections.add(head);
+        throwCherry(sections);
+    }
 
-			for(Point x: segmentySnake)
-				if(head.getX() == x.getX() && head.getY() == x.getY() && x != head
-						|| head.getX() >=30 || head.getX() < 0 || head.getY() >=30 || head.getY() < 0 )
-				{
-					timer.stop();
-					JOptionPane.showMessageDialog(null, "\t\tGra skoñczona! Naciœnij OK aby spróbowaæ ponownie.");
-					restartGry(segmentySnake);
-					break;
-				}
-		}
-	}
+    public static void restart(ArrayList<Point> sections) {
+        sections.removeAll(sections);
+        sections.add(new Point(3, 2));
+        snake.head = new Point(3, 3);
+        sections.add(snake.head);
+        direction = DOWN;
+        snake.timer.start();
+    }
 
-	public void keyPressed(KeyEvent e)
-	{
-		if(e.getKeyCode() == KeyEvent.VK_A && direction!= RIGHT)
-			direction = LEFT;
-		if(e.getKeyCode() == KeyEvent.VK_S && direction!= UP)
-			direction = DOWN;
-		if(e.getKeyCode() == KeyEvent.VK_D && direction!= LEFT)
-			direction = RIGHT;
-		if(e.getKeyCode() == KeyEvent.VK_W && direction!= DOWN)
-			direction = UP;
-		
-		if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-			System.exit(0);
-	}
+    public static void throwCherry(ArrayList<Point> sections) {
+        cherry = new Point(random.nextInt(30), random.nextInt(30));
+        for (Point x : sections)
+            if (cherry.getX() == x.getX() && cherry.getY() == x.getY())
+                throwCherry(sections);
+    }
 
-	public void keyReleased(KeyEvent e) {}
-	public void keyTyped(KeyEvent e) {}
+    public static void main(String [] args) {
+        snake = new Snake();
+        JOptionPane.showMessageDialog(null, "\t\tWitaj w grze SNAKE!"
+            + "\nU¿ywaj nastêpuj¹cych klawiszy aby zmieniæ kierunek ruchu wê¿a:"
+            + "\n W - do góry"
+            + "\n S - w dó³ "
+            + "\n A - w lewo"
+            + "\n D - w prawo."
+            + "\n POWODZENIA!");
+        snake.timer.start();
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+        renderPanel.repaint();
+        speedInterval++;
+        if (speedInterval % 4 == 0) {
+            move();
+            checkCherry();
+            checkIfGameEnds();
+        }
+    }
+
+    private void move() {
+        if (direction == DOWN)
+            beforeHead = new Point((int) head.getX(), (int) head.getY() + 1);
+        if (direction == UP)
+            beforeHead = new Point((int) head.getX(), (int) head.getY() - 1);
+        if (direction == LEFT)
+            beforeHead = new Point((int) head.getX() - 1, (int) head.getY());
+        if (direction == RIGHT)
+            beforeHead = new Point((int) head.getX() + 1, (int) head.getY());
+        sections.add(beforeHead);
+        Point vicarial = head;
+        head = beforeHead;
+        beforeHead = vicarial;
+    }
+
+    private void checkCherry() {
+        if (head.getX() == cherry.getX() && head.getY() == cherry.getY())
+            throwCherry(sections);
+        else
+            sections.remove(0);
+    }
+
+    private void checkIfGameEnds() {
+        for (Point x : sections)
+            if (head.getX() == x.getX() && head.getY() == x.getY() && x != head
+                || head.getX() >= 30 || head.getX() < 0 || head.getY() >= 30 || head.getY() < 0) {
+                timer.stop();
+                JOptionPane.showMessageDialog(null, "\t\tGra skoñczona! Naciœnij OK aby spróbowaæ ponownie.");
+                restart(sections);
+                break;
+            }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_A && direction != RIGHT)
+            direction = LEFT;
+        if (e.getKeyCode() == KeyEvent.VK_S && direction != UP)
+            direction = DOWN;
+        if (e.getKeyCode() == KeyEvent.VK_D && direction != LEFT)
+            direction = RIGHT;
+        if (e.getKeyCode() == KeyEvent.VK_W && direction != DOWN)
+            direction = UP;
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+            System.exit(0);
+    }
+
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public static Snake getInstance() {
+        return snake;
+    }
+
+    public static Point getCherry() {
+        return cherry;
+    }
+
+    public ArrayList<Point> getSections() {
+        return sections;
+    }
 }
